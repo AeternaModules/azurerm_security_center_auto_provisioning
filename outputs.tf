341cee5a06838a98bbf3e38a7fff191acca0459c
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:02:14 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "security_center_auto_provisionings" {
+  description = "All security_center_auto_provisioning resources"
+  value       = azurerm_security_center_auto_provisioning.security_center_auto_provisionings
+}
+
