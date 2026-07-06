6457a08d7bbfd749cb2fc05f5d6a0f5b45662af0
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:49 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,8 @@ output "security_center_auto_provisionings" {
   description = "All security_center_auto_provisioning resources"
   value       = azurerm_security_center_auto_provisioning.security_center_auto_provisionings
 }
+output "security_center_auto_provisionings_auto_provision" {
+  description = "List of auto_provision values across all security_center_auto_provisionings"
+  value       = [for k, v in azurerm_security_center_auto_provisioning.security_center_auto_provisionings : v.auto_provision]
+}
 
