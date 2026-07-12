749329377cf4ad7560025668617084a35fff561a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "security_center_auto_provisionings_id" {
+  description = "Map of id values across all security_center_auto_provisionings, keyed the same as var.security_center_auto_provisionings"
+  value       = { for k, v in azurerm_security_center_auto_provisioning.security_center_auto_provisionings : k => v.id }
+}
 output "security_center_auto_provisionings_auto_provision" {
   description = "Map of auto_provision values across all security_center_auto_provisionings, keyed the same as var.security_center_auto_provisionings"
   value       = { for k, v in azurerm_security_center_auto_provisioning.security_center_auto_provisionings : k => v.auto_provision }
